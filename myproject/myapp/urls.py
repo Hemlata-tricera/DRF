cac35b6b66d6090b1137481fe8895ff77e2d7d37
Hemlata <hemlata.raghuvanshi@tricera.tech>
2025-01-14 23:59:23 +0530

    Add studentlist and studentdetail view

---
+++ myproject/myapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .import views
+
+urlpatterns=[
+    path('list', views.student_list_view, name='student_list'),
+    path('<int:pk>', views.student_detail_view, name='student_detail'),
+
+
+
+]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from .models import Student, Course
+from django.http import JsonResponse
 # Create your views here.
+
+
+def student_list_view(request):
+    students = Student.objects.all()
+    data = {
+        'students': list(students.values())
+    }
+    return JsonResponse(data)
+
+def student_detail_view(request, pk):
+    student = Student.objects.get(pk=pk)
+    data = {
+        'name': student.name,
+        'email': student.email
+    }
+    return JsonResponse(data)
+
+
+
