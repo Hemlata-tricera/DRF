6df6c12ce9654024c27d5a703799b9009b2a9384
Hemlata <hemlata.raghuvanshi@tricera.tech>
2025-01-18 12:21:22 +0530

    Add neccessary fields in models: Student and Course

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -5,14 +5,25 @@ from django.db import models
 
 class Course(models.Model):
     name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10, unique=True, default=101)
+
 
     def __str__(self):
         return self.name
 
 
 class Student(models.Model):
-    name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    dob = models.DateField(null=False, blank=False, default='2025-01-01')
     email = models.EmailField()
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    enrollment_date = models.DateField(auto_now_add=True, null=True, blank=True)
+    courses = models.ManyToManyField('Course', blank=True)
+    gender = models.CharField(max_length=10, choices=[('Male', 'Male'), ('Female', 'Female'), ('Other', 'Other')],
+                              blank=True, null=True)
+    student_id = models.CharField(max_length=20, unique=True, null=True, blank=True)  # Unique student ID
 
     def __str__(self):
-        return self.name
+        return self.first_name
