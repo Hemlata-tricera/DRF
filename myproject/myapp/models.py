e33cbf9978ffbb3ffc56e30a500ba1e1ea7ac3d5
Hemlata <hemlata.raghuvanshi@tricera.tech>
2025-01-18 12:46:35 +0530

    Alter Student model fields:dob,student_id and enrollment_date

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from datetime import date
+
 
 # Create your models here.
 
@@ -15,15 +17,15 @@ class Course(models.Model):
 class Student(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100, blank=True, null=True)
-    dob = models.DateField(null=False, blank=False, default='2025-01-01')
+    dob = models.DateField(null=False, blank=False, default='2000-01-01')
     email = models.EmailField()
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
-    enrollment_date = models.DateField(auto_now_add=True, null=True, blank=True)
+    enrollment_date = models.DateField(null=False, blank=False, default=date.today)
     courses = models.ManyToManyField('Course', blank=True)
     gender = models.CharField(max_length=10, choices=[('Male', 'Male'), ('Female', 'Female'), ('Other', 'Other')],
                               blank=True, null=True)
-    student_id = models.CharField(max_length=20, unique=True, null=True, blank=True)  # Unique student ID
+    student_id = models.CharField(max_length=20, unique=True, null=False, blank=False)  # Unique student ID
 
     def __str__(self):
         return self.first_name
